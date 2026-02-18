d530766abc425581dec29205501f47939db5db90
indecisivenitin <ahir6346@gmail.com>
2026-02-18 21:26:08 +0530

    deployed frontend base url again

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,10 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : "https://chatapp-v2-0r6l.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
